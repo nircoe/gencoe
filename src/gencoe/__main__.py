8b1201485be5bd824c00ec81f62aa87f152038a8
nircoe <nircoe@gmail.com>
2025-12-17 19:57:12 +0200

    [Core]: Initial gencoe architecture

---
+++ src/gencoe/__main__.py
@@ -0,0 +1,8 @@
+"""gencoe CLI entry point"""
+
+def main():
+    print("gencoe")
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/gencoe/init.py
@@ -0,0 +1,5 @@
+"""Initialize a gamecoe gamedev project"""
+
+def generate():
+    """Generate a new empty gamecoe gamedev project"""
+    pass
